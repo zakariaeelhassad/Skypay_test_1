22af09e705fb0a4c9dd6bb030654f2806c550654
Zakariae El Hassad <zakariaelhassad031@gmail.com>
2025-12-12 14:59:42 +0100

    Add InsufficientFundsException for withdrawal validation

---
+++ src/main/java/org/example/exception/InsufficientFundsException.java
@@ -0,0 +1,7 @@
+package org.example.exception;
+
+public class InsufficientFundsException extends Exception {
+    public InsufficientFundsException(String message) {
+        super(message);
+    }
+}
