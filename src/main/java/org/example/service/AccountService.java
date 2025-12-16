5fc97991a9c0536cd2fa628da59d66da74623f2d
Zakariae El Hassad <zakariaelhassad031@gmail.com>
2025-12-16 12:21:30 +0100

    refactor: update AccountService interface methods

--- src/main/java/org/example/service/AccountService.java
+++ src/main/java/org/example/service/AccountService.java
@@ -1,4 +1,7 @@
 package org.example.service;
 
 public interface AccountService {
+    void deposit(int amount, String date);
+    void withdraw(int amount, String date);
+    void printStatement();
 }
