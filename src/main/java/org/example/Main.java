cb4fbcf89b48472a8da2ba7a32b8b11c91b9f118
Zakariae El Hassad <zakariaelhassad031@gmail.com>
2025-12-16 12:22:03 +0100

    refactor: update Main class to use new AccountService implementation

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,30 +1,18 @@
 package org.example;
 
-import org.example.entity.Account;
-import org.example.exception.InsufficientFundsException;
-import org.example.exception.InvalidAmountException;
-import org.example.exception.InvalidDateException;
-import org.example.presentation.StatementPrinter;
-import org.example.service.TransactionService;
+import org.example.service.AccountService;
+import org.example.service.AccountServiceImpl;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Account account = new Account();
-        TransactionService transactionService = new TransactionService();
-        StatementPrinter printer = new StatementPrinter();
+        AccountService account = new AccountServiceImpl();
 
-        try {
-            transactionService.performTransaction(account, 1000, "30-12-2026", "DEPOSIT");
-            transactionService.performTransaction(account, 2000, "30-12-2026", "DEPOSIT");
+        account.deposit(1000, "10-01-2012");
+        account.deposit(2000, "13-01-2012");
+        account.withdraw(500, "14-01-2012");
 
-            transactionService.performTransaction(account, 500, "30-12-2026", "WITHDRAW");
-
-        } catch (InvalidAmountException | InsufficientFundsException | InvalidDateException e) {
-            System.out.println("Error: " + e.getMessage());
-        }
-
-        printer.print(account);
+        account.printStatement();
     }
 }
