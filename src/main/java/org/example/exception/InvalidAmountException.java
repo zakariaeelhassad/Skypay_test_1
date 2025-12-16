3ecd0446a1d7dbfb16f72b16f225340ad9f0dab1
Zakariae El Hassad <zakariaelhassad031@gmail.com>
2025-12-16 12:21:20 +0100

    refactor: improve InvalidAmountException error handling

--- src/main/java/org/example/exception/InvalidAmountException.java
+++ src/main/java/org/example/exception/InvalidAmountException.java
@@ -1,7 +1,11 @@
 package org.example.exception;
 
-public class InvalidAmountException extends Exception{
+public class InvalidAmountException extends RuntimeException {
     public InvalidAmountException(String message) {
         super(message);
+    }
+
+    public InvalidAmountException(String message, Throwable cause) {
+        super(message, cause);
     }
 }
